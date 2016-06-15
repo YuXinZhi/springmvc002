f43c69ee3a3edce82770dfdded6a44d0166a13f3
Ben <whoyumengmeng@gmail.com>
2016-06-15 15:42:34 +0800

    init

--- src/cn/ben/springmvc/TestController.java
+++ src/cn/ben/springmvc/TestController.java
@@ -1,20 +1,10 @@
 package cn.ben.springmvc;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-public class TestController extends AbstractController {
-
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
-		System.out.println("hello Spring MVC");
-
-		// viewName不上jsp的名称而是前缀与后缀之间的路径字符串
-		return new ModelAndView("index");
-	}
+//用来标注当前类是springmvc的控制层放类
+//相同级别注解的还有 Component,Service,Repository
+@Controller
+public class TestController {
 
 }
